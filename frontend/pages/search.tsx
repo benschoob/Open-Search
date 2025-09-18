1aa7d94ac77414d9ef1a455b21066a187aabfb64
Ben Schubert <benschubert2002@gmail.com>
2025-09-18 13:35:16 -0600

    refactored frontend source files for routing

--- frontend/pages/search.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './style.module.css';
-
-export default function Home() {
-  return (
-    <h1>AAAAAAAAA</h1>
-  );
-}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import styles from 'style.module.css';
+
+export default function Index() {
+  return (
+    <h1>AAAAAAAAA</h1>
+  );
+}
